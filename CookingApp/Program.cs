e0703d8d3b507c0b39de5d0faab893b6d04d0879
PrgaTest123 <marintest0@gmail.com>
2022-09-29 20:33:05 +0200

    Add project files.

---
+++ CookingApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CookingApp
+{
+    class Lasagna
+    {
+        private int ExpectedCookTimeInMinuts = 40;
+        private int MinutesPerLayer = 2;
+
+        public int ExpectedMinutesInOven() => ExpectedCookTimeInMinuts;
+
+        public int RemainingMinutesInOven(int min) => ExpectedMinutesInOven() - min;
+        public int PreparationTimeInMinutes(int layers) => MinutesPerLayer * layers;
+        public int ElapsedTimeInMinutes(int numberOfLayers, int numberOfMinutes) => PreparationTimeInMinutes(numberOfLayers) + numberOfMinutes;
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lasagna = new Lasagna();
+
+           Console.WriteLine( lasagna.ExpectedMinutesInOven());
+            Console.WriteLine(lasagna.RemainingMinutesInOven(30));
+            Console.WriteLine(lasagna.PreparationTimeInMinutes(2));
+            Console.WriteLine(lasagna.ElapsedTimeInMinutes(3, 20));
+        }
+
+      
+
+
+
+
+    }
+
+    
+}
